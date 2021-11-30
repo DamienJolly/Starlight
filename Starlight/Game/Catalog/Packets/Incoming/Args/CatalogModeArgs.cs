31c291e9dd3d412a136635ea5c4bce4354e74acf
Damien Jolly <DamienJolly@hotmail.com>
2021-11-30 01:03:52 +0000

    Make catalog mode always uppercase

--- Starlight/Game/Catalog/Packets/Incoming/Args/CatalogModeArgs.cs
+++ Starlight/Game/Catalog/Packets/Incoming/Args/CatalogModeArgs.cs
@@ -3,13 +3,13 @@ using Starlight.API.Communication.Messages.Protocols;
 
 namespace Starlight.Game.Catalog.Packets.Incoming.Args
 {
-    public class CatalogModeArgs : IMessageArgs
-    {
-        public string Mode { get; private set; }
+	public class CatalogModeArgs : IMessageArgs
+	{
+		public string Mode { get; private set; }
 
-        public void Parse(IClientMessage message)
-        {
-            Mode = message.ReadString();
-        }
-    }
+		public void Parse(IClientMessage message)
+		{
+			Mode = message.ReadString().ToUpper();
+		}
+	}
 }
